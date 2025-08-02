1fbfe783afd8e01229e190d34c66efcbe4449b01
JanF <jfirlus@outlook.de>
2025-08-02 18:43:27 +0200

    Sensor Entität eingeführt

--- custom_components/platerecognizer/sensor.py
+++ custom_components/platerecognizer/sensor.py
@@ -1,10 +1,10 @@
-from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.entity import SensorEntity
 from .const import DOMAIN
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     async_add_entities([PlateRecognizerSensor(hass)], True)
 
-class PlateRecognizerSensor(Entity):
+class PlateRecognizerSensor(SensorEntity):
     def __init__(self, hass):
         self._attr_name = "Plate Recognizer Last Plate"
         self._attr_unique_id = f"{DOMAIN}_last_plate"
@@ -18,3 +18,8 @@ class PlateRecognizerSensor(Entity):
     @property
     def state(self):
         return self._state
+    
+    @property
+    def unique_id(self):
+        return f"{DOMAIN}_last_plate"
+
